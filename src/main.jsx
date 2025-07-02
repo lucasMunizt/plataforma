d4635cecc5f6611c6ff8559861407aeacf6d16c3
Lucas Muniz <87603728+lucasMunizt@users.noreply.github.com>
2025-07-02 15:10:34 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
-
 import App from './App.jsx';
 import Login from './telas/Login.jsx';
 import Calendario from './telas/Calendario.jsx';
