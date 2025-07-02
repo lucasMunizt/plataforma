3c835f36e82659b5340f61827a94578852474460
Lucas Muniz <87603728+lucasMunizt@users.noreply.github.com>
2025-07-02 16:00:41 -0300

    alteração

--- src/DadosGet.jsx
+++ src/DadosGet.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './DadosGet.css';
-import Header from '../../componentes/header/Header';
-import Card from '../../componentes/card/Card';
+import Header from './componentes/header/Header';
+import Card from './componentes/card/Card';
 import {Search} from 'lucide-react'
 import { getDadosGarantia, getDadosOrdemServico,getDadosPesquisados } from '../../services/GetDadosBanco';
 
